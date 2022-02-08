74e29567b31e3d642a86e813d9b612729514ab01
Tuana Sevik <91959281+tuanasevik@users.noreply.github.com>
2022-02-08 15:24:14 +0100

    Add files via upload

---
+++ slideshow/js/script.js
@@ -0,0 +1,18 @@
+
+function leftImg() {
+    let temp = document.images[4].src;
+    document.images[4].src = document.images[3].src;
+    document.images[3].src = document.images[2].src;
+    document.images[2].src = document.images[1].src;
+    document.images[1].src = document.images[0].src;
+    document.images[0].src = temp;
+}
+
+function rightImg() {
+    let temp = document.images[0].src;
+    document.images[0].src = document.images[1].src;
+    document.images[1].src = document.images[2].src;
+    document.images[2].src = document.images[3].src;
+    document.images[3].src = document.images[4].src;
+    document.images[4].src = temp;
+}
